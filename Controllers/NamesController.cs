7e6cc21cb40e9688c9a797a78d412d0b1fb2fc55
Jahmal <Jahmal23@gmail.com>
2014-01-12 17:14:33 -0500

    now you can view names

--- Controllers/NamesController.cs
+++ Controllers/NamesController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Zipper.Models;
+using Zipper.BLL;
 
 namespace Zipper.Controllers
 {
@@ -13,7 +15,8 @@ namespace Zipper.Controllers
 
         public ActionResult ViewAll()
         {
-            return View();
+            NameSource namesToSearch = ZipperBLL.GetAllNames();
+            return View(namesToSearch);
         }
 
     }
